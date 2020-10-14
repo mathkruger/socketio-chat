42ca1d4bdf52564714c8e3aa6fbc9990789916c8
Matheus Kruger <matheus.silva@datapar.com.br>
2020-10-14 20:47:15 -0300

    adicionando suporte a ASCII art, mensagem de audio e imagens

--- index.js
+++ index.js
@@ -6,46 +6,46 @@ connections = [];
 users = [];
 cores = [];
 
-app.get('/', function(req,res) {
+app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/notification.mp3', function(req,res) {
+app.get('/notification.mp3', function (req, res) {
 	res.sendFile(__dirname + '/public/notification.mp3');
 });
 
-app.get('/soundmanager2.js', function(req,res) {
+app.get('/soundmanager2.js', function (req, res) {
 	res.sendFile(__dirname + '/public/soundmanager2.js');
 });
 
-app.get('/soundmanager2_debug.swf', function(req,res) {
+app.get('/soundmanager2_debug.swf', function (req, res) {
 	res.sendFile(__dirname + '/public/soundmanager2_debug.swf');
 });
 
-app.get('/soundmanager2.swf', function(req,res) {
+app.get('/soundmanager2.swf', function (req, res) {
 	res.sendFile(__dirname + '/public/soundmanager2.swf');
 });
 
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
 	connections.push(socket);
 	writeNewConnection('c');
-	
-	socket.on('disconnect', function(data) {
-		users.splice(users.indexOf(socket.username),1);
-		cores.splice(cores.indexOf(socket.cor),1);
+
+	socket.on('disconnect', function (data) {
+		users.splice(users.indexOf(socket.username), 1);
+		cores.splice(cores.indexOf(socket.cor), 1);
 		updateUsernames();
 		io.emit('saiu', socket.username);
 
-		connections.splice(connections.indexOf(socket),1);
+		connections.splice(connections.indexOf(socket), 1);
 
 		writeNewConnection('d');
 	});
 
-	socket.on('send message', function(data) {
-		io.emit('new message', {message: data, username: socket.username, cor: socket.cor});
+	socket.on('send message', function (data) {
+		io.emit('new message', { message: data, username: socket.username, cor: socket.cor });
 	});
 
-	socket.on('new user', function(data, callback) {
+	socket.on('new user', function (data, callback) {
 		callback(true);
 
 		socket.username = data.username;
@@ -58,28 +58,36 @@ io.on('connection', function(socket) {
 		updateUsernames();
 	});
 
-	function updateUsernames(){
-		io.emit('get users', {username: users, cor: cores});
+	socket.on('radio', function (blob) {
+		io.emit('new message', { username: socket.username, cor: socket.cor, blob: blob, tipo: 'audio' });
+	});
+
+	socket.on('image', function (blob) {
+		io.emit('new message', { username: socket.username, cor: socket.cor, blob: blob, tipo: 'img' });
+	});
+
+	function updateUsernames() {
+		io.emit('get users', { username: users, cor: cores });
 	};
 
-	function writeNewConnection(estado){
+	function writeNewConnection(estado) {
 		var stringEstado = '';
 
-		if(estado == 'c')
+		if (estado == 'c')
 			stringEstado = "Conectado:";
 		else
 			stringEstado = "Desconectado:";
 
 
-		if(connections.length === 1)
-			console.log(''+stringEstado,connections.length,'socket.');
+		if (connections.length === 1)
+			console.log('' + stringEstado, connections.length, 'socket.');
 		else
-			console.log(''+stringEstado,connections.length,'sockets.');
+			console.log('' + stringEstado, connections.length, 'sockets.');
 	}
 
 });
 
-http.listen(3000, function() {
+http.listen(1232, function () {
 	console.log('Chat: rodando na porta 3000...');
 	console.log('acesse http://localhost:3000');
 });
