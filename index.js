58cbd7d3aff298dc22a5bb43534d11e90226528f
Glitch (mathkruger-socketio-chat) <none>
2020-10-14 20:56:17 +0000

    🚩📌 Checkpoint ./public/index.html:23801859/27737 ./index.js:23801859/8594

--- index.js
+++ index.js
@@ -1,85 +1,92 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io').listen(http);
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io").listen(http);
 
 connections = [];
 users = [];
 cores = [];
 
-app.get('/', function(req,res) {
-	res.sendFile(__dirname + '/public/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-app.get('/notification.mp3', function(req,res) {
-	res.sendFile(__dirname + '/public/notification.mp3');
+app.get("/notification.mp3", function(req, res) {
+  res.sendFile(__dirname + "/public/notification.mp3");
 });
 
-app.get('/soundmanager2.js', function(req,res) {
-	res.sendFile(__dirname + '/public/soundmanager2.js');
+app.get("/soundmanager2.js", function(req, res) {
+  res.sendFile(__dirname + "/public/soundmanager2.js");
 });
 
-app.get('/soundmanager2_debug.swf', function(req,res) {
-	res.sendFile(__dirname + '/public/soundmanager2_debug.swf');
+app.get("/soundmanager2_debug.swf", function(req, res) {
+  res.sendFile(__dirname + "/public/soundmanager2_debug.swf");
 });
 
-app.get('/soundmanager2.swf', function(req,res) {
-	res.sendFile(__dirname + '/public/soundmanager2.swf');
+app.get("/soundmanager2.swf", function(req, res) {
+  res.sendFile(__dirname + "/public/soundmanager2.swf");
 });
 
-io.on('connection', function(socket) {
-	connections.push(socket);
-	writeNewConnection('c');
-	
-	socket.on('disconnect', function(data) {
-		users.splice(users.indexOf(socket.username),1);
-		cores.splice(cores.indexOf(socket.cor),1);
-		updateUsernames();
-		io.emit('saiu', socket.username);
+io.on("connection", function(socket) {
+  connections.push(socket);
+  writeNewConnection("c");
 
-		connections.splice(connections.indexOf(socket),1);
+  socket.on("disconnect", function(data) {
+    users.splice(users.indexOf(socket.username), 1);
+    cores.splice(cores.indexOf(socket.cor), 1);
+    updateUsernames();
+    io.emit("saiu", socket.username);
 
-		writeNewConnection('d');
-	});
+    connections.splice(connections.indexOf(socket), 1);
 
-	socket.on('send message', function(data) {
-		io.emit('new message', {message: data, username: socket.username, cor: socket.cor});
-	});
+    writeNewConnection("d");
+  });
 
-	socket.on('new user', function(data, callback) {
-		callback(true);
+  socket.on("send message", function(data) {
+    io.emit("new message", {
+      message: data,
+      username: socket.username,
+      cor: socket.cor
+    });
+  });
 
-		socket.username = data.username;
-		socket.cor = data.cor;
-		users.push(socket.username);
-		cores.push(socket.cor);
+  socket.on("new user", function(data, callback) {
+    callback(true);
 
-		io.emit('logou', socket.username);
+    socket.username = data.username;
+    socket.cor = data.cor;
+    users.push(socket.username);
+    cores.push(socket.cor);
 
-		updateUsernames();
-	});
+    io.emit("logou", socket.username);
 
-	function updateUsernames(){
-		io.emit('get users', {username: users, cor: cores});
-	};
+    updateUsernames();
+  });
 
-	function writeNewConnection(estado){
-		var stringEstado = '';
+  socket.on("radio", function(blob) {
+    io.emit("new message", {
+      username: socket.username,
+      cor: socket.cor,
+      blob: blob
+    });
+  });
 
-		if(estado == 'c')
-			stringEstado = "Conectado:";
-		else
-			stringEstado = "Desconectado:";
+  function updateUsernames() {
+    io.emit("get users", { username: users, cor: cores });
+  }
 
+  function writeNewConnection(estado) {
+    var stringEstado = "";
 
-		if(connections.length === 1)
-			console.log(''+stringEstado,connections.length,'socket.');
-		else
-			console.log(''+stringEstado,connections.length,'sockets.');
-	}
+    if (estado == "c") stringEstado = "Conectado:";
+    else stringEstado = "Desconectado:";
 
+    if (connections.length === 1)
+      console.log("" + stringEstado, connections.length, "socket.");
+    else console.log("" + stringEstado, connections.length, "sockets.");
+  }
 });
 
 http.listen(3000, function() {
-	console.log('Chat: rodando na porta 3000...');
-	console.log('acesse http://localhost:3000');
+  console.log("Chat: rodando na porta 3000...");
+  console.log("acesse http://localhost:3000");
 });
